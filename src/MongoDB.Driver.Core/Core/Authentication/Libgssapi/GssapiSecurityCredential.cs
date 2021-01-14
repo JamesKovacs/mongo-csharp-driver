9c6227adbc370e5ce9c0106959b8aea2026e9d37
James Kovacs <jkovacs@post.harvard.edu>
2021-01-14 16:57:09 -0700

    WIP

--- src/MongoDB.Driver.Core/Core/Authentication/Libgssapi/GssapiSecurityCredential.cs
+++ src/MongoDB.Driver.Core/Core/Authentication/Libgssapi/GssapiSecurityCredential.cs
@@ -1,3 +1,18 @@
+/* Copyright 2021-present MongoDB Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 using System;
 using System.Runtime.InteropServices;
 using System.Security;
@@ -11,7 +26,7 @@ namespace MongoDB.Driver.Core.Authentication.Libgssapi
     public class GssapiSecurityCredential : SafeHandle
     {
         /// <summary>
-        /// Acquires the TGT from the KDC.
+        /// Acquires the TGT from the KDC with provided password or keytab (if password is null).
         /// </summary>
         /// <param name="username"></param>
         /// <param name="password"></param>
@@ -60,6 +75,16 @@ namespace MongoDB.Driver.Core.Authentication.Libgssapi
                     NativeMethods.ReleaseName(out uint _, gssCanonicalizedName);
                 }
             }
+        }
+
+        /// <summary>
+        /// Acquires the TGT from the KDC using keytab.
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        public static GssapiSecurityCredential Acquire(string username)
+        {
+            return Acquire(username, null);
         }
 
         private GssapiSecurityCredential(IntPtr credentialHandle) : base(credentialHandle, true)
--- tests/MongoDB.Driver.Core.Tests/Core/Authentication/Libgssapi/GssapiSecurityCredentialTests.cs
+++ tests/MongoDB.Driver.Core.Tests/Core/Authentication/Libgssapi/GssapiSecurityCredentialTests.cs
@@ -1,6 +1,6 @@
-using System.Security;
 using FluentAssertions;
 using MongoDB.Driver.Core.Authentication.Libgssapi;
+using MongoDB.Shared;
 using Xunit;
 
 namespace MongoDB.Driver.Core.Tests.Core.Authentication.Libgssapi
@@ -12,13 +12,18 @@ namespace MongoDB.Driver.Core.Tests.Core.Authentication.Libgssapi
         {
             const string username = "drivers@LDAPTEST.10GEN.CC";
             const string password = "powerbook17";
-            var securePassword = new SecureString();
-            foreach (var c in password)
-            {
-                securePassword.AppendChar(c);
-            }
+            var securePassword = SecureStringHelper.ToSecureString(password);
 
             var credential = GssapiSecurityCredential.Acquire(username, securePassword);
+            credential.Should().NotBeNull();
+        }
+
+        [Fact]
+        public void Should_acquire_gssapi_security_credential_with_username_only()
+        {
+            const string username = "drivers@LDAPTEST.10GEN.CC";
+
+            var credential = GssapiSecurityCredential.Acquire(username);
             credential.Should().NotBeNull();
         }
     }
