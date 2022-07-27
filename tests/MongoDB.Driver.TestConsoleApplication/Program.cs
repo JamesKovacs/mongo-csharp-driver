a692975a900907c7e83f6bf67316aa0f6c0b2128
James Kovacs <jkovacs@post.harvard.edu>
2022-07-27 15:24:21 -0600

    Minor updates.

--- tests/MongoDB.Driver.TestConsoleApplication/Program.cs
+++ tests/MongoDB.Driver.TestConsoleApplication/Program.cs
@@ -3,11 +3,9 @@ using System.Collections.Generic;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDB.Driver.Encryption;
-using MongoDB.Driver.Linq;
 
 var uri = "mongodb+srv://james:aVEQ3ry7Do3O5xS6@cluster0.1foxh.mongodb.net/test?retryWrites=true&w=majority";
 var settings = MongoClientSettings.FromConnectionString(uri);
-settings.LinqProvider = LinqProvider.V3;
 
 var localMasterKey = Convert.FromBase64String("Mng0NCt4ZHVUYUJCa1kxNkVyNUR1QURhZ2h2UzR2d2RrZzh0cFBwM3R6NmdWMDFBMUN3YkQ5aXRRMkhGRGdQV09wOGVNYUMxT2k3NjZKelhaQmRCZGJkTXVyZG9uSjFk");
 var kmsProviders = new Dictionary<string, IReadOnlyDictionary<string, object>>();
@@ -21,8 +19,8 @@ var autoEncryptionOptions = new AutoEncryptionOptions(keyVaultNamespace, kmsProv
 
 settings.AutoEncryptionOptions = autoEncryptionOptions;
 var client = new MongoClient(settings);
-var db = client.GetDatabase("test");
-var collection = db.GetCollection<BsonDocument>("sample_mflix");
+var db = client.GetDatabase("sample_mflix");
+var collection = db.GetCollection<BsonDocument>("movies");
 
 // Simple search example
 var searchStage = @"
@@ -42,11 +40,10 @@ var projection = @"
 }";
 var pipeline = new EmptyPipelineDefinition<BsonDocument>()
     .AppendStage<BsonDocument, BsonDocument, BsonDocument>(searchStage)
-    .Limit(5)
     .Project(projection);
 var moviesAboutBaseball = collection.Aggregate(pipeline).ToList();
 
-Console.WriteLine("Movies about baseball:");
+Console.WriteLine($"{moviesAboutBaseball.Count} movies about baseball:");
 foreach (var movie in moviesAboutBaseball)
 {
     Console.WriteLine($"  {movie}");
