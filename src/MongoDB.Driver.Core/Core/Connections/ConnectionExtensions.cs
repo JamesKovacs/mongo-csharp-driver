5d3e0725b6045b684a504b72de510ee3432d88ad
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2024-02-27 15:21:15 -0800

    Rollback unnecessary changes

--- src/MongoDB.Driver.Core/Core/Connections/ConnectionExtensions.cs
+++ src/MongoDB.Driver.Core/Core/Connections/ConnectionExtensions.cs
@@ -13,8 +13,13 @@
 * limitations under the License.
 */
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver.Core.Misc;
 using MongoDB.Driver.Core.WireProtocol.Messages;
 using MongoDB.Driver.Core.WireProtocol.Messages.Encoders;
--- tests/MongoDB.Driver.Core.Tests/Core/Configuration/ClusterBuilderTests.cs
+++ tests/MongoDB.Driver.Core.Tests/Core/Configuration/ClusterBuilderTests.cs
@@ -15,6 +15,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentAssertions;
 using MongoDB.Bson.TestHelpers;
 using MongoDB.Driver.Core.Authentication;
